4f3065f50f86ed3feefd8ee731cbeeb0fcac2e0f
WOLNOME <143981493+WOLNOME@users.noreply.github.com>
2024-10-02 12:09:09 +0900

    完成

--- WinApp.h
+++ WinApp.h
@@ -1,6 +1,11 @@
 #pragma once
+#include <windows.h>
+#include <wrl.h>
+
 class WinApp
 {
+public:
+	static LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
 public:
 	void Initialize();
 	void Update();
--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <Windows.h>
 #include <string>
 #include <format>
-#include <cstdint>
 #include <d3d12.h>
 #include <dxgi1_6.h>
 #include <cassert>
@@ -31,7 +30,6 @@
 #include "externals/imgui/imgui.h"
 #include "externals/imgui/imgui_impl_dx12.h"
 #include "externals/imgui/imgui_impl_win32.h"
-extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 
 #pragma comment(lib,"d3d12.lib")
@@ -112,28 +110,6 @@ struct SoundData
 	//バッファのサイズ
 	unsigned int bufferSize;
 };
-
-
-
-//ウィンドウプロシージャ
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
-	//マウスの操作をできるようImGuiに伝達
-	if (ImGui_ImplWin32_WndProcHandler(hwnd, msg, wparam, lparam)) {
-		return true;
-	}
-
-	//メッセージに応じてゲーム固有の処理を行う
-	switch (msg) {
-		//ウィンドウが破棄された
-	case WM_DESTROY:
-		//OSに対して、アプリの終了を伝える
-		PostQuitMessage(0);
-		return 0;
-	}
-
-	//標準メッセージ処理を行う
-	return DefWindowProc(hwnd, msg, wparam, lparam);
-}
 
 //ログ
 void Log(const std::string& message) {
@@ -790,48 +766,16 @@ void SoundPlayWave(IXAudio2* xAudio2, const SoundData& soundData)
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
-	//COMの初期化
-	HRESULT hr = CoInitializeEx(0, COINIT_MULTITHREADED);
 	//解放処理確認用
 	D3DResourceLeakChecker leakCheck;
 
-	WNDCLASS wc{};
-	//ウィンドウプロシージャ
-	wc.lpfnWndProc = WindowProc;
-	//ウィンドウクラス名(何でもいい)
-	wc.lpszClassName = L"CG2WindowClass";
-	//インスタンスハンドル
-	wc.hInstance = GetModuleHandle(nullptr);
-	//カーソル
-	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	//ポインタ
+	WinApp* winApp = nullptr;
+	//WindowsAPIの初期化
+	winApp = new WinApp();
+	winApp->Initialize();
 
-	//ウィンドウクラスを登録する
-	RegisterClass(&wc);
-
-	//クライアント領域のサイズ
-	const int32_t kClientWidth = 1280;
-	const int32_t kClientHeight = 720;
-
-	//ウィンドウサイズを表す構造体にクライアント領域を入れる
-	RECT wrc = { 0,0,kClientWidth,kClientHeight };
-
-	//クライアント領域を元に実際のサイズにwrcを変更してもらう
-	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
-
-	//ウィンドウの生成
-	HWND hwnd = CreateWindow(
-		wc.lpszClassName,     //利用するクラス名
-		L"CG2",               //タイトルバーの文字(何でもいい)
-		WS_OVERLAPPEDWINDOW,  //よく見るウィンドウスタイル
-		CW_USEDEFAULT,        //表示X座標(Windowsに任せる)
-		CW_USEDEFAULT,        //表示Y座標(Windowsに任せる)
-		wrc.right - wrc.left, //ウィンドウ横幅
-		wrc.bottom - wrc.top, //ウィンドウ縦幅
-		nullptr,              //親ウィンドウハンドル
-		nullptr,              //メニューハンドル
-		wc.hInstance,         //インスタンスハンドル
-		nullptr);             //オプション
-
+	
 #ifdef _DEBUG
 	Microsoft::WRL::ComPtr<ID3D12Debug1> debugContoroller = nullptr;
 	if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugContoroller)))) {
@@ -841,10 +785,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		debugContoroller->SetEnableGPUBasedValidation(TRUE);
 	}
 #endif // _DEBUG
-
-
-	//ウィンドウを表示する
-	ShowWindow(hwnd, SW_SHOW);
 
 	//DXGIファクトリーの生成
 	Microsoft::WRL::ComPtr<IDXGIFactory7> dxgiFactory = nullptr;
@@ -2406,6 +2346,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	//音声データ
 	SoundUnload(&soundData1);
 
+	delete winApp;
 	delete input;
 
 
