94a2dcb5a9d037e6437c20fe4fac7bcc8dcf3ab2
WOLNOME <143981493+WOLNOME@users.noreply.github.com>
2024-10-31 15:01:32 +0900

    ｓ

---
+++ project/TitleScene.h
@@ -0,0 +1,23 @@
+#pragma once
+class TitleScene
+{
+public:
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+	/// <summary>
+	/// 終了時
+	/// </summary>
+	void Finalize();
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+private://メンバ変数
+};
+
