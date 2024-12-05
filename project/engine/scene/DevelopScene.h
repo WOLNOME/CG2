e2fb45f1d780f38f752e749b2060bda08de84210
WOLNOME <143981493+WOLNOME@users.noreply.github.com>
2024-12-06 01:16:06 +0900

    あ

--- project/engine/scene/DevelopScene.h
+++ project/engine/scene/DevelopScene.h
@@ -41,39 +41,8 @@ private://メンバ変数
 	std::unique_ptr<DevelopCamera> camera;
 	Vector3 cameraTranslate = { 0.0f,0.0f,-15.0f };
 	Vector3 cameraRotate = { 0.0f,0.0f,0.0f };
-	//平行光源
-	std::unique_ptr<DirectionalLight> dirLight;
-	//点光源
-	std::unique_ptr<PointLight> pointLight;
-	std::unique_ptr<LineDrawer> plMark;
-	bool isDrawPLMark = false;
-	std::unique_ptr<PointLight> pointLight2;
-	std::unique_ptr<LineDrawer> plMark2;
-	bool isDrawPLMark2 = false;
-	//スポットライト
-	std::unique_ptr<SpotLight> spotLight;
-	std::unique_ptr<LineDrawer> slMark;
-	bool isDrawSLMark = false;
-
-	uint32_t textureHandleSprite_ = 0u;
-	std::unique_ptr<Sprite> sprite_ = nullptr;
-	uint32_t textureHandleSprite2_ = 0u;
-	std::unique_ptr<Sprite> sprite2_ = nullptr;
-	Vector2 sprite2Position;
-
-	WorldTransform wtAxis_;
-	std::unique_ptr<Object3d> axis_ = nullptr;
-
-	WorldTransform wtTerrain_;
-	std::unique_ptr<Object3d> terrain_ = nullptr;
-
-
+	
 	std::unique_ptr<Particle> particle_ = nullptr;
 
-	std::unique_ptr<LineDrawer> line_ = nullptr;
-
-	bool isDrawSphere_ = false;
-	std::unique_ptr<Audio> audio_ = nullptr;
-	float volume = 0.5f;
 };
 
